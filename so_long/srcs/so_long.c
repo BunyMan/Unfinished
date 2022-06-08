972926beddf9ac8992b3c6f8f4eb6b3b95b61bff
Buny Man <jpbuny@gmail.com>
2022-06-08 18:07:58 +0100

    Moved ALOT of stuff to struct

--- so_long/srcs/so_long.c
+++ so_long/srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: jbuny-fe <jbuny-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 15:54:49 by jbuny-fe          #+#    #+#             */
-/*   Updated: 2022/06/08 16:24:35 by jbuny-fe         ###   ########.fr       */
+/*   Updated: 2022/06/08 18:04:03 by jbuny-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 int	main(void)
 {
-	win_s ptr;
+	t_win_s	ptr;
 	void	*img_ptr;
 	int		img_x;
 	int		img_y;
-	
+	char	*path;
+
+	path = "includes/atk.xpm";
 	win_printer(&ptr);
-	img_ptr = mlx_xpm_file_to_image(ptr.mlx, "includes/atk.xpm", &img_x, &img_y);
+	img_ptr = mlx_xpm_file_to_image(ptr.mlx, path, &img_x, &img_y);
 	mlx_put_image_to_window(ptr.mlx, ptr.win, img_ptr, 320, 320);
 	ptr.moves = 0;
 	mlx_key_hook(ptr.win, deal_key, &ptr);
 	mlx_hook(ptr.win, 17, 1L << 2, &close_x_button, &ptr);
 	mlx_loop(ptr.mlx);
-	return(0);
+	return (0);
 }
