cd40fbfbf135949f6214f01ef29edd769afd13f0
Buny Man <jpbuny@gmail.com>
2022-06-08 18:16:49 +0100

    A lot of norm fixes

--- so_long/srcs/window_printer.c
+++ so_long/srcs/window_printer.c
@@ -6,7 +6,7 @@
 /*   By: jbuny-fe <jbuny-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:05:21 by jbuny-fe          #+#    #+#             */
-/*   Updated: 2022/06/08 18:05:02 by jbuny-fe         ###   ########.fr       */
+/*   Updated: 2022/06/08 18:15:45 by jbuny-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 void	win_printer(t_win_s *ptr)
 {
-	// void	*img_ptr;
 	int		img_x;
 	int		img_y;
-	// int		ptr->win_x;
-	// int		win_y;
-	// int		nix;
-	// int		niy;
+	char	*b_path;
 
+	b_path = "includes/back.xpm";
 	ptr->mlx = mlx_init();
-	ptr->img_ptr = mlx_xpm_file_to_image(ptr->mlx, "includes/back.xpm", &img_x, &img_y);
+	ptr->img_ptr = mlx_xpm_file_to_image(ptr->mlx, b_path, &img_x, &img_y);
 	ptr->win_x = img_x * 10;
 	ptr->win_y = img_y * 10;
-	ptr->win = mlx_new_window(ptr->mlx, ptr->win_x, ptr->win_y, "Testes do mlx");
+	ptr->win = mlx_new_window(ptr->mlx, ptr->win_x, ptr->win_y, "Testing");
 	ptr->nix = 0;
 	ptr->niy = 0;
 	while (ptr->nix < ptr->win_x && ptr->niy < ptr->win_y)
@@ -38,6 +35,5 @@ void	win_printer(t_win_s *ptr)
 			ptr->niy += img_y;
 			ptr->nix = 0;
 		}
-
 	}
 }
