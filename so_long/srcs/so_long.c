f18c9070606a5b49d7801f7aad8abe16de02fd2d
Buny Man <jpbuny@gmail.com>
2022-06-08 15:13:12 +0100

    background with a junkie

--- so_long/srcs/so_long.c
+++ so_long/srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: jbuny-fe <jbuny-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 15:54:49 by jbuny-fe          #+#    #+#             */
-/*   Updated: 2022/06/08 14:57:21 by jbuny-fe         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:09:14 by jbuny-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int	main(void)
 	int		niy;
 
 	ptr.mlx = mlx_init();
-	img_ptr = mlx_xpm_file_to_image(ptr.mlx, "includes/atk.xpm", &img_x, &img_y);
+	img_ptr = mlx_xpm_file_to_image(ptr.mlx, "includes/back.xpm", &img_x, &img_y);
 	win_x = img_x * 10;
 	win_y = img_y * 10;
 	ptr.win = mlx_new_window(ptr.mlx, win_x, win_y, "Testes do mlx");
@@ -96,7 +96,8 @@ int	main(void)
 		}
 		
 	}
-	mlx_put_image_to_window(ptr.mlx, ptr.win, img_ptr, 500, 500);
+	img_ptr = mlx_xpm_file_to_image(ptr.mlx, "includes/atk.xpm", &img_x, &img_y);
+	mlx_put_image_to_window(ptr.mlx, ptr.win, img_ptr, 320, 320);
 	mlx_key_hook(ptr.win, deal_key, &ptr);
 	mlx_hook(ptr.win, 17, 1L << 2, &close_x_button, &ptr);
 	mlx_loop(ptr.mlx);
